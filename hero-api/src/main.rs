e81148a84f31428318e033a8543f6ec49a8c111f
sachin <sacsand.k@gmail.com>
2021-01-13 19:40:42 +0800

    🚧 Work In Progress

--- hero-api/src/main.rs
+++ hero-api/src/main.rs
@@ -1,2 +1,43 @@
-#![feature(plugin)]
-#![]
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+#[macro_use] extern crate serde_derive;
+
+//extern crate rocket;
+use rocket_contrib::{Json, Value};
+mod hero;
+use hero::{Hero};
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[post("/", data = "<hero>")]
+fn create(hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[get("/")]
+fn read() -> Json<Value> {
+    Json(json!([
+        "hero 1", 
+        "hero 2"
+    ]))
+}
+
+#[put("/<id>", data = "<hero>")]
+fn update(id: i32, hero: Json<Hero>) -> Json<Hero> {
+    hero
+}
+
+#[delete("/<id>")]
+fn delete(id: i32) -> Json<Value> {
+    Json(json!({"status": "ok"}))
+}
+
+fn main() {
+    rocket::ignite()
+        .mount("/hello", routes![hello])
+        .launch();
+}
